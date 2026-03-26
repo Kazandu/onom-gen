49a07c8643505e99f57466ca65495464a8c386bf
Kazandu <github@kazandu.moe>
2026-03-26 03:48:43 +0100

    sketch006: reworked to add transparency, curated pattern ruleset list vs rand, fixed shift to right, custom cell color

--- sketch006.js
+++ sketch006.js
@@ -7,11 +7,37 @@ var exportCount = 0;
 var exportMax = 600;
 var exportSessionID = "";
 
+let lastRule = -1;
+
+const params = {
+renderBG: true,
+bgColor: '#000000',
+cellColor: '#00FF64',
+ruleset: 'Random',
+};
+
+const rulesets = {
+  Random: 'rand',
+  Curated: [1, 3, 15, 18, 22, 30, 45, 60, 73, 90, 105, 107, 109, 110, 121, 126, 129, 150, 182, 190, 250] //Source for chosen Rules https://atlas.wolfram.com/01/01/
+};
+
 var guiConfig = [
   { variable: 'w', min: 2, max: 50, step: 1, name: 'Cell Size', onFinishChange: function(){ initCA(); } },
+  { object: params, variable: 'ruleset', options: Object.keys(rulesets), name: 'Pattern Ruleset'},
+  { object: params, variable: 'cellColor', type: 'color', name: 'Cell color'},
+  { object: params, variable: 'renderBG', name: 'Render Background', onChange: updateBackground},
+  { object: params, variable: 'bgColor', type: 'color', name: 'Background', onChange: updateBackground },
   { variable: 'exportMax', min: 60, max: 1200, step: 1, name: 'Export Frames' },
   { variable: 'startExport', name: 'Start Export', type: 'function' }
 ];
+
+function updateBackground() {
+  if (params.renderBG) {
+    background(params.bgColor); 
+  } else {
+    clear();
+  }
+}
 
 function setup() {
   let c = createCanvas(1920, 1080);
@@ -26,21 +52,23 @@ function setup() {
 }
 
 function initCA() {
-  cells = Array(floor(width / w));
-  for (let i = 0; i < cells.length; i++) {
-    cells[i] = 0;
-  }
-  cells[floor(cells.length / 2)] = 1;
-  fill(0);
-  rect(0, 0, width, height);
+  let numCols = floor(width / w);
+  cells= Array(numCols).fill(0);
+  let middle = floor(numCols / 2);
+  
+  generation = 0;
+  updateBackground();
 }
 
 function draw() {
+  let totalGridWidth = cells.length * w;
+  let screenOffset = (width - totalGridWidth) / 2;
+
   for (let i = 0; i < cells.length; i++) {
     if (cells[i] === 1) {
-      fill(0, 255, 100); // Neon Green
+      fill(params.cellColor)
       noStroke();
-      rect(i * w, generation * w, w, w);
+      rect(screenOffset + (i * w), generation * w, w + 1, w + 1);
     }
   }
 
@@ -49,13 +77,29 @@ function draw() {
   
   // 画面下まで行ったらリセット
   if (generation * w > height) {
-      fill(0);
-      rect(0, 0, width, height);
-      generation = 0;
-      cells = Array(floor(width / w)).fill(0);
-      cells[floor(cells.length / 2)] = 1;
-      // ルールをランダム変更
-      for(let i=0; i<8; i++) caRules[i] = floor(random(2));
+    updateBackground();
+    generation = 0;
+
+    cells = Array(floor(width / w)).fill(0);
+    cells[floor(cells.length / 2)] = 1;
+    
+    let chosenRuleNumber;
+    let selRuleSet = rulesets[params.ruleset]
+    
+    do {
+      if (selRuleSet === 'rand') {
+        chosenRuleNumber = floor(random(256));
+      } else {
+        chosenRuleNumber = random(selRuleSet);
+      }
+    } while (chosenRuleNumber == lastRule);
+
+    lastRule = chosenRuleNumber;
+
+    // ルールをランダム変更
+    for(let i=0; i<8; i++) {
+       caRules[7 - i] = (chosenRuleNumber >> i) & 1;
+    }
   }
 
   // 書き出し処理
@@ -71,10 +115,10 @@ function draw() {
 
 function generate() {
   let nextgen = Array(cells.length);
-  for (let i = 1; i < cells.length - 1; i++) {
-    let left = cells[i - 1];
+  for (let i = 0; i < cells.length; i++) {
+    let left = cells[(i - 1 + cells.length) % cells.length];
     let me = cells[i];
-    let right = cells[i + 1];
+    let right = cells[(i + 1) % cells.length];
     nextgen[i] = rules(left, me, right);
   }
   cells = nextgen;
